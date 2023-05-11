f149ac3db3c513d908da8900c76a20f8e10d3b46
Yusuf TEKMİL <yusuftekmil.yt@outlook.com.tr>
2023-05-11 04:15:44 +0300

    v1 basic Project

---
+++ hello_Webapi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using hello_Webapi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello_Webapi.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController :ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            var result=  new ResponseModel()
+            { HttpStatus = 200 , Message="Hello ASP.Net Core Web API" };
+            return Ok(result);
+        }
+    }
+}
